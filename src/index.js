99bff0ff725296fa1c148c438afea7ea79cba9f2
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2021-04-17 12:08:48 -0600

    Servidor basico

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const puerto = 3000 || process.env.PORT;
 
-app.get("/maestro", (req, res) => {
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/error.html");
+});
+
+//maestro
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/maestro.html");
 });
 
