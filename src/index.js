e4d2623381de24c644519982a5d1c5c652e50993
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2021-04-17 12:22:48 -0600

    app use error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app = express();
 const puerto = 3000 || process.env.PORT;
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname + "/public/error.html");
-});
-
 //maestro
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/maestro.html");
@@ -21,6 +17,10 @@ app.get("/esclavo2", (req, res) => {
 
 app.get("/esclavo3", (req, res) => {
   res.sendFile(__dirname + "/public/esclavo3.html");
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/error.html");
 });
 
 app.listen(puerto, () => {
