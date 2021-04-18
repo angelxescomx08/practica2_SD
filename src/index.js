11f2badc32de87c624f5c0e96b597a1e102c0d08
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2021-04-18 17:09:26 -0500

    cambiando orden del puerto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const puerto = 3000 || process.env.PORT;
+const puerto = process.env.PORT||3000;
 const Reloj = require(__dirname + "/public/js/reloj.js");
 const random = require(__dirname+'/public/js/aleatorio.js');
 
